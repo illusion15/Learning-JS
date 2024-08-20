37a8420729d6ef6fbb14760d3e385c0b36f4610a
impriyanshu15 <itzpriyanshu15@gmail.com>
2024-08-20 21:34:43 +0530

    Uploading some imp points from  my today's learnign of JS

---
+++ Practice/updating value of a var/script.js
@@ -0,0 +1,17 @@
+let welcomeMessage = 'Hello Friends';
+console.log('hello bhai');
+console.log(welcomeMessage);
+
+
+let a = 3;
+let b = 4;
+console.log(a + b);
+
+// updating value of var a 
+a = b + 1;
+console.log(a);
+
+let i = 5;
+console.log(i);
+
+i += 5;  // i = i + 5;
